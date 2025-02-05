1fed33a095c6fd7fb599edcc9f957d814f47f9c8
Rosie <jayjaygames40@gmail.com>
2025-02-04 22:13:46 -0600

    Removed scene creator program because I have no functional use for it now. Sayonara, my sweet prince

--- tools/scene-creator/include/error.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ERROR_H_
-#define _ERROR_H_
-
-void error_log(const char *error_format, ...);
-void error_check(const int function_return, const int desired_return,
-		 const char *error_message);
-
-#endif /* _ERROR_H_ */
